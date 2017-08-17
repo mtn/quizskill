4585392ca3af12f139a684a4cbf709b7f9f28188
Michael Noronha <michaeltnoronha@gmail.com>
2017-08-17 16:00:10 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+import quizlet
+import json
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def quizlet_handler():
+
+    if request.method == 'POST':
+        raw_data = request.get_json()
+
+        query = raw_data["request"]["intent"]["slots"]["query"]
+
+        with open("secrets.json","r") as secrets:
+            secrets = json.loads(secrets.read())
+
+            client = quizlet.QuizletClient(client_id=secrets['client_id'])
+
+            query_result = client.api.search.sets.get(params={ 'q': query })
+            print(query_result)
+
+    return 'Hello, World!'
