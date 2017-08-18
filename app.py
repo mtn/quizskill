615b3741c398adc3e6a5af4fe716c34374493429
Michael Noronha <michaeltnoronha@gmail.com>
2017-08-18 19:39:19 -0400

    Add some more handlers

--- app.py
+++ app.py
@@ -9,21 +9,31 @@ ask = Ask(app, '/')
 # Should be true on production
 app.config['ASK_VERIFY_REQUESTS'] = False
 
+@ask.launch
+def launch_handler():
+    return question('Welcome to Study Helper')
+
+@ask.on_session_started
+def new_session():
+    log.info('New session started')
+
 @ask.intent("StudyIntent")
 def study_handler(topic):
-    speech_text = "Let's study %s" % topic
+    speech_text = "Alright, let's study %s" % topic
+
+    client_secret = environ.get('CLIENT_ID')
+    client = quizlet.QuizletClient(client_id=client_secret)
+
+    query_result = client.api.search.sets.get(params={ 'q': query })
+
     return statement(speech_text)
+
+@ask.session_ended
+def session_end_handler():
+    return "", 200
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
-
-    #
-    #         client_secret = environ.get('CLIENT_ID')
-    #         client = quizlet.QuizletClient(client_id=client_secret)
-    #
-    #         query_result = client.api.search.sets.get(params={ 'q': query })
     #         print(query_result)
-    #
-    # return 'Hello, World!'
