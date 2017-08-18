2712db659519f166472a1e4874a31cdaa3ef076b
keiranking <me@keiranking.com>
2017-08-18 12:52:29 -0400

    Changed JSON.

--- app.py
+++ app.py
@@ -11,13 +11,14 @@ def quizlet_handler():
     if request.method == 'POST':
         raw_data = request.get_json()
 
-        query = raw_data["request"]["intent"]["slots"]["query"]
+        if raw_data["request"]["intent"]["name"] == "StudyIntent":
+            query = raw_data["request"]["intent"]["slots"]["topic"]["value"]
 
-        client_secret = environ.get('CLIENT_ID')
-        client = quizlet.QuizletClient(client_id=client_secret)
+            client_secret = environ.get('CLIENT_ID')
+            client = quizlet.QuizletClient(client_id=client_secret)
 
-        query_result = client.api.search.sets.get(params={ 'q': query })
-        print(query_result)
+            query_result = client.api.search.sets.get(params={ 'q': query })
+            print(query_result)
 
     return 'Hello, World!'
 
