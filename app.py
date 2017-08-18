c50896ebb0d20b0786fce2c17485c180b22de131
Michael Noronha <michaeltnoronha@gmail.com>
2017-08-18 16:19:25 -0400

    Test for heroku

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
+from flask_ask import Ask, statement
 from flask import Flask, request
 from os import environ
-from alexa import Alexa
 import quizlet
 
-alexa = Alexa("Quizlet")
-
 app = Flask(__name__)
-
-@app.route('/', methods=['GET', 'POST'])
-def main_handler():
+ask = Ask(app, '/')
 
 
-    if request.method == 'POST':
-        raw_data = request.get_json()
+if __name__ == '__main__':
+    app.run()
 
-        return alexa.route(raw_data)
+# @ask.intent('StudyIntent')
+# def hello():
+    # return statement("StudyIntent")
 
+# @ask.intent("StudyIntent")
+# def study_handler(topic):
+#     # speech_text = "Let's study %s" % topic
+#     speech_text = "This was invoked"
+#     return statement(speech_text)
         #
     #     if raw_data["request"]["intent"]["name"] == "StudyIntent":
     #         query = raw_data["request"]["intent"]["slots"]["topic"]["value"]
@@ -26,13 +29,4 @@ def main_handler():
     #         query_result = client.api.search.sets.get(params={ 'q': query })
     #         print(query_result)
     #
-    return 'Hello, World!'
-
-@alexa.intent("StudyIntent", mapping={"topic": "topic"})
-def study_handler(session, topic):
-    return alexa.response.statement(str(topic))
-
-
-
-if __name__ == '__main__':
-    app.run()
+    # return 'Hello, World!'
