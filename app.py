a47db8641ed104a172b252830b352c4d264f12ef
Michael Noronha <michaeltnoronha@gmail.com>
2017-08-22 18:09:48 -0400

    Rever to a working version

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask_ask import Ask, statement
 from flask import Flask, request
 from os import environ
 import quizlet
-import random
 
 app = Flask(__name__)
 ask = Ask(app, '/')
@@ -10,29 +9,21 @@ ask = Ask(app, '/')
 # Should be true on production
 app.config['ASK_VERIFY_REQUESTS'] = False
 
-@ask.launch
-def launch_handler():
-    return question('Welcome to Study Helper')
-
-@ask.on_session_started
-def new_session():
-    log.info('New session started')
-
 @ask.intent("StudyIntent")
 def study_handler(topic):
-    client_secret = environ.get('CLIENT_ID')
-    client = quizlet.QuizletClient(client_id=client_secret)
-
-    query_result = client.api.search.sets.get(params={ 'q': query })
-
-    return question(render_template())
-
-@ask.session_ended
-def session_end_handler():
-    log.info('Session ended')
-    return "", 200
+    speech_text = "Let's study %s" % topic
+    return statement(speech_text)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
 
+
+    #
+    #         client_secret = environ.get('CLIENT_ID')
+    #         client = quizlet.QuizletClient(client_id=client_secret)
+    #
+    #         query_result = client.api.search.sets.get(params={ 'q': query })
+    #         print(query_result)
+    #
+    # return 'Hello, World!'
