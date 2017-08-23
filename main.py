2a02cfdcbc63144c83cfac9d81519ae8c92b02c1
Michael Noronha <michaeltnoronha@gmail.com>
2017-08-23 01:55:14 -0400

    Gets terms

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ from flask_ask import Ask, statement, question, session
 from flask import Flask, request, render_template
 from os import environ
 import quizlet
+import random
 
 app = Flask(__name__)
 ask = Ask(app, '/')
 
 # Should be true on production
 app.config['ASK_VERIFY_REQUESTS'] = False
-client_secret = environ.get('CLIENT_ID')
-client = quizlet.QuizletClient(client_id=client_secret)
 
 @ask.launch
 def launch_handler():
@@ -18,13 +17,36 @@ def launch_handler():
 @ask.intent("StudyIntent")
 def study_handler(topic):
     session.attributes['topic'] = topic
-    return question(render_template('confirm_topic',topic=topic))
+
+    client_secret = environ.get('CLIENT_ID')
+    client = quizlet.QuizletClient(client_id=client_secret)
+
+    query_result = client.api.search.sets.get(params={ 'q': topic })
+
+    while True:
+        ind = random.randint(0,len(query_result['sets']))
+        session.attributes['set_id'] = query_result['sets'][ind]['id']
+        user_set = client.api.sets.get(session.attributes['set_id'])
+
+        if not user_set['terms'][0]['definition'] == '':
+            break
+
+    session.attributes['last_ind'] = 0
+    return question(render_template('confirm_topic',
+                    topic=topic,
+                    term=user_set['terms'][0]['term']))
 
 @ask.intent("GetTerm")
 def term_handler():
-    query = session.attributes['topic']
-    query_result = client.api.search.sets.get(params={ 'q': query })
-    return(query_result)
+    if 'topic' not in session.attributes:
+        return question("Sorry, what do you want me to help you study?")
+
+    topic = session.attributes['topic']
+
+    if 'id' not in session.attributes:
+        query_result = client.api.search.sets.get(params={ 'q': topic })
+        ind = random.randint(0,len(query_result['sets']))
+        session.attributes['id'] = query_result['sets'][ind]['id']
 
 
 
